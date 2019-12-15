8b9db42861a601589b88700542dc12660c087ca8
Ju1ice <Julia.dzh@gmail.com>
2019-12-15 16:03:54 -0500

    Add Bid

--- src/main/java/com/sidehustle/controllers/BidController.java
+++ src/main/java/com/sidehustle/controllers/BidController.java
@@ -1,15 +1,20 @@
 package com.sidehustle.controllers;
 
+import java.io.Console;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sidehustle.beans.Bid;
+import com.sidehustle.beans.Task;
 import com.sidehustle.beans.User;
 import com.sidehustle.data.BidRepository;
 
@@ -20,9 +25,17 @@ public class BidController {
 	private BidRepository repository;
 	
 	
-	@PostMapping(value = "/bids/add", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Bid save(@RequestBody Bid bid,HttpSession session) {
+	@PostMapping(value = "/bids/add/{uid}/{tid}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Bid save(@PathVariable int uid,  @PathVariable int tid, @RequestBody Bid bid) {
 		//log.info(task);
+		User user = new User();
+		user.setUid(uid);
+		Task task = new Task();
+		task.setTid(tid);
+		bid.setUser(user);
+		bid.setTask(task);
+		System.out.println("BID USER: "+ uid);
+		System.out.println("BID TASK: "+ tid);
 		System.out.println("This is Bid: " + bid);
 		//return null;
 		return repository.save(bid);
