02e29918ed75f2094686f12d7de9d673bb85e5ec
Ju1ice <Julia.dzh@gmail.com>
2019-12-16 16:40:15 -0500

    delete added

--- src/main/java/com/sidehustle/controllers/BidController.java
+++ src/main/java/com/sidehustle/controllers/BidController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,4 +41,10 @@ public class BidController {
 		//return null;
 		return repository.save(bid);
 	}
+	
+	@DeleteMapping(value = "/bids/remove/{id}")
+	public void RemoveBidById(@PathVariable int id) {
+		repository.deleteById(id);
+	}
+	
 }
